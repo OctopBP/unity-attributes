9a3ec6c2357ccae4257b3bafbe7cfcdcaed80b0c
Boris Proshin <kaktusao@mail.ru>
2023-07-19 03:02:26 +0300

    `1.1.0`. Add MonoReadonly

--- UnityAttributes/MonoReadonly/MonoReadonly.Analyzer.cs
+++ UnityAttributes/MonoReadonly/MonoReadonly.Analyzer.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
@@ -7,19 +8,30 @@ using Microsoft.CodeAnalysis.Diagnostics;
 namespace UnityAttributes.MonoReadonly;
 
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
-public class MonoReadonlyAnalyzer : DiagnosticAnalyzer
-{
+public class MonoReadonlyAnalyzer : DiagnosticAnalyzer {
+    static readonly string[] AllowedMethodsNames = { "Awake", "OnEnable", "Start", "Reset" };
+
     const string DiagnosticId = "MonoReadonlyAnalyzer";
 
     static readonly DiagnosticDescriptor AssignmentRule = new DiagnosticDescriptor(
         DiagnosticId,
-        "Property assignment detected",
-        "Property assignment detected at '{0}'",
-        "Usage",
+        title: "Field assignment detected",
+        messageFormat: $"Field '{{0}}' can be assigned only in {string.Join(", ", AllowedMethodsNames)} methods.",
+        category: "Usage",
         DiagnosticSeverity.Error,
         isEnabledByDefault: true,
-        "Assignment to properties should be avoided",
-        "https://yourwebsite.com/diagnostics/property-assignment");
+        description: "Assignment to fields should be avoided"
+    );
+    
+    static readonly DiagnosticDescriptor DuplicateAssignmentRule = new DiagnosticDescriptor(
+        DiagnosticId,
+        title: "Duplicate field assignment detected",
+        messageFormat: "Field '{0}' assigned more than in one place.",
+        category: "Usage",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true,
+        description: "Assignment duplication to fields should be avoided"
+    );
 
     public override void Initialize(AnalysisContext context)
     {
@@ -37,6 +49,8 @@ public class MonoReadonlyAnalyzer : DiagnosticAnalyzer
 
         // Find all assignments in the syntax tree
         var assignmentExpressions = root.DescendantNodesAndSelf().OfType<AssignmentExpressionSyntax>();
+
+        var propertiesNames = new List<string>();
         
         foreach (var assignmentExpression in assignmentExpressions)
         {
@@ -48,10 +62,19 @@ public class MonoReadonlyAnalyzer : DiagnosticAnalyzer
                     attr.AttributeClass != null
                     && attr.AttributeClass.ToDisplayString() == MonoReadonlyGenerator.AttributeName
             )) {
-                // Report a diagnostic for each assignment to the TestMessage-annotated property
+                if (propertiesNames.Contains(property.Name))
+                {
+                    var diagnostic = Diagnostic.Create(DuplicateAssignmentRule, assignmentExpression.GetLocation(), assignmentExpression.Left.ToString());
+                    context.ReportDiagnostic(diagnostic);
+                }
+                else
+                {
+                    propertiesNames.Add(property.Name);
+                }
                 
+                // Report a diagnostic for each assignment to property
                 var methodDeclaration = assignmentExpression.Ancestors().OfType<MethodDeclarationSyntax>().FirstOrDefault();
-                if (methodDeclaration != null && methodDeclaration.Identifier.ValueText != "Awake" && methodDeclaration.Identifier.ValueText != "Start")
+                if (methodDeclaration != null && !AllowedMethodsNames.Contains(methodDeclaration.Identifier.ValueText))
                 {
                     var diagnostic = Diagnostic.Create(AssignmentRule, assignmentExpression.GetLocation(), assignmentExpression.Left.ToString());
                     context.ReportDiagnostic(diagnostic);
@@ -60,5 +83,5 @@ public class MonoReadonlyAnalyzer : DiagnosticAnalyzer
         }
     }
 
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(AssignmentRule);
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(AssignmentRule, DuplicateAssignmentRule);
 }
--- UnityAttributes/MonoReadonly/MonoReadonlyGenerator.cs
+++ UnityAttributes/MonoReadonly/MonoReadonlyGenerator.cs
@@ -1,13 +1,10 @@
 using Microsoft.CodeAnalysis;
-using UnityAttributes.Common;
 
 namespace UnityAttributes.MonoReadonly;
 
 [Generator]
 public partial class MonoReadonlyGenerator : ISourceGenerator
 {
-	static readonly string generatedCodeAttribute = typeof(MonoReadonlyGenerator).generatedCodeAttribute();
-
 	public void Initialize(GeneratorInitializationContext context)
 	{
 		context.RegisterForPostInitialization(i => i.AddSource($"{AttributeName}.g.cs", attributeText));
